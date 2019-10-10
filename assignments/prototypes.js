7528228e9c87d3787adb9a3cc5c53e3fab04f231
Daniel Morales <daniel.a.moralesalt@gmail.com>
2019-10-10 13:15:17 -0400

    Working on Stretch goals

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -127,3 +127,16 @@ console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
 // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.
 // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
 // * Create two new objects, one a villain and one a hero and fight it out with methods!
+
+function Villain(villainAttrs) {
+  Humanoid.call(this, villainAttrs);
+}
+
+Villain.prototype = Object.create(Humanoid.prototype);
+Villain.prototype.attack = function() {};
+
+function Hero(heroAttrs) {
+  Humanoid.call(this, heroAttrs);
+}
+
+Hero.prototype = Object.create(Humanoid.prototype);
